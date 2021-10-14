206392bd247d34f4b2ef7b34a7c9289ceb9294fa
Rebecca Bartels <rebeccakbartels@gmail.com>
2021-10-14 16:31:41 -0700

    Update countlog.sql

--- countlog.sql
+++ countlog.sql
@@ -1,10 +1,15 @@
 SELECT 
 x.MDP_CompanyKey,
+x.TotalCustomers,
+x.TotalSales,
 x.RowNum
 FROM (  
      Select 
      sc.MDP_CompanyKey,
+     sc.MDPTotalSales,
+     sc.TotalSales,
      cc.TotalCustomers,
+     cc.MDPTotalCustomers,
      row_number() over (partition by sc.MDP_CompanyKey order by sc.MDP_CompanyKey desc) rowNum
      FROM SalesByMDPKeyCount sc
      INNER JOIN CustomerByMDPKeyCount cc ON cc.MDP_CompanyKey = sc.MDP_CompanyKey
